7dbafb07d0582c279e690f2ca958134afed7bfd4
Michel de Freitas Herrero <michelherrero@focustextil.loc.br>
2023-08-29 11:30:39 -0300

    corrigindo erros

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import mongoose from "mongoose";
 import crypto from "crypto";
 import generateToken from "@mcorange9/generate-token";
 import User from "./schemas/User.js";
+import bodyParser from "body-parser";
 const app = express();
 const gen = new generateToken();
 class App {
     run() {
+
         try {
             mongoose.connect(process.env.DBURL,
                 {
@@ -23,6 +25,12 @@ class App {
             console.log("---- ERRO AO CONECTAR AO BANCO DE DADOS ----")
         }
 
+        // application/json
+        app.use(bodyParser.json());
+
+        //  application/x-www-form-urlencoded
+        app.use(bodyParser.urlencoded({ extended: true }));
+
         app.post("/api/cadastro", (req, res) => {
             const pssw = req.body.pssw;
             const email = req.body.email;
@@ -34,18 +42,18 @@ class App {
                 } else {
                     try {
                         const psswhash = crypto.createHash('md5').update(pssw).digest('hex');
-                        token = gen.generateToken(15);
+                        const token = gen.generateToken(15);
                         var createUser = new User({
                             token: token,
                             userEmail: email,
                             userPassword: psswhash,
                         })
                         createUser.save().then(() => {
-                            res.status("200");
+                            res.status(200);
                             return res.json({ status: '200', feedback: "Usuário cadastrado com sucesso!", token: token, hashpssw: psswhash });
                         });
                     } catch (error) {
-                        res.status("500");
+                        res.status(500);
                         return res.json({ status: '500', feedback: "Erro interno", error: error });
                     }
 
@@ -56,27 +64,27 @@ class App {
         app.post("/api/login", async (req, res) => {
             const pssw = req.body.pssw;
             const email = req.body.email;
-            const dbuser = await User.findOne({userEmail: email});
+            const dbuser = await User.findOne({ userEmail: email });
             //verifica se o email existe.
             User.exists({ userEmail: email }).then(exists => {
                 if (exists) {
                     try {
                         const psswhash = crypto.createHash('md5').update(pssw).digest('hex');
                         const token = dbuser.token;
-                        if(psswhash == dbuser.userPassword) {
-                            res.status("200");
+                        if (psswhash == dbuser.userPassword) {
+                            res.status(200);
                             return res.json({ status: '200', feedback: "Usuário validado com sucesso!", token: token });
                         } else {
-                            res.status("400");
-                            return res.json({ status: '400', feedback: "Senha errada!" }); 
+                            res.status(400);
+                            return res.json({ status: '400', feedback: "Senha errada!" });
                         }
                     } catch (error) {
-                        res.status("500");
+                        res.status(500);
                         return res.json({ status: '500', feedback: "Erro interno", error: error });
                     }
                 } else {
-                    res.status("400");
-                    return res.json({ status: '400', feedback: "Usuário não cadastrado!" }); 
+                    res.status(400);
+                    return res.json({ status: '400', feedback: "Usuário não cadastrado!" });
                 }
             })
         });
